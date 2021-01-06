ca74d871169954a152f5d52d8293ac9a979b127e
Linkim <kimchaelin13@gmail.com>
2021-01-07 01:45:16 +0900

    0106

---
+++ memo-coffee/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <memo-app></memo-app>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+import MemoApp from './components/MemoApp'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    MemoApp,
+  }
+}
+</script>
+
+<style>
+@import "./styles/reset.css";
+
+#app {
+  width: 560px;
+  margin: 0 auto;
+}
+</style>
---
+++ memo-coffee/src/components/MemoApp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="memo-app">
+    <memo-form></memo-form>
+    <memo></memo>
+  </div>  
+</template>
+
+<script>
+import Memo from './Memo.vue';
+import MemoForm from './MemoForm.vue'
+
+
+export default {
+  components: { Memo,MemoForm },
+  name:'MemoApp',
+  data() {
+    return {
+      memos:[],
+    }
+  },
+  created() {
+    this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
+  }
+}
+</script>
+
+<style>
+
+</style>
